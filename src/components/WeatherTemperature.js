32d73e1a4a5f7f52a64a735a5a9a8c46376ca337
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-05-27 20:20:03 -0600

    Add case for the climate

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature=()=>(
+const getWeatherState=(weatherState)=> {
+    switch (weatherState) {
+        case "cloud":
+            return (
+                <WeatherIcons name="cloud" size="2x" />
+            );
+        default:
+            return (
+                <WeatherIcons name="sleet" size="2x" />
+            );
+    }
+};
+const WeatherTemperature=({temperature, weatherState})=>(
     <div>
-        <WeatherIcons name="cloud" size="2x" />
-        <span>12</span>
+        {getWeatherState(weatherState) }
+        <span> {`${temperature} C°`}</span>
     </div>
 );
 
