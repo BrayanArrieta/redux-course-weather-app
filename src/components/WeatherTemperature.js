3c179124e845b200850020c7abe7f4250e04b60f
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-05-27 18:59:22 -0600

    Add Weather-Icons by means of cdn

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import WeatherIcons from 'react-weathericons';
 
 const WeatherTemperature=()=>(
     <div>
+        <WeatherIcons name="cloud" size="2x" />
         <span>12</span>
     </div>
 );
