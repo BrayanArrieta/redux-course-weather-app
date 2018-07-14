4230572d0e705f8dc78fe1f9fa3faead06799f28
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-07-13 19:06:28 -0600

    Added styles for the application

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Location from "./Location";
 import WeatherData from "./WeatherData";
-
+import './styles.css';
 const WeatherLocation = () => (
-    <div>
+    <div className='weatherLocationCont'>
         <Location city={"Buenos Aires"}/>
         <WeatherData/>
     </div>
