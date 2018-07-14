470cabdfe63aa8d684d0f8773669b30b4a63dcad
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-07-13 21:35:36 -0600

    PropTypes in the shade way
    Can be used for objects

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './styles.css';
+const data = {
+    temperature:21,
+    weatherState:"cloud",
+    humidity:80,
+    wind:"10m/s"
+}
+
 const WeatherLocation = () => (
     <div className='weatherLocationCont'>
         <Location city={"Buenos Aires"}/>
-        <WeatherData/>
+        <WeatherData data={data}/>
     </div>
 );
 
