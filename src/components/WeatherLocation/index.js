758e64786d2cae1984c254324ac577bec71c993d
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-07-14 11:40:04 -0600

    Component type Class Component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './styles.css';
+
 const data = {
-    temperature:21,
-    weatherState:"cloud",
-    humidity:80,
-    wind:"10m/s"
+    temperature: 21,
+    weatherState: "cloud",
+    humidity: 80,
+    wind: "10m/s"
 }
 
-const WeatherLocation = () => (
-    <div className='weatherLocationCont'>
-        <Location city={"Buenos Aires"}/>
-        <WeatherData data={data}/>
-    </div>
-);
+class WeatherLocation extends Component {
+    render() {
+        return (
+            <div className='weatherLocationCont'>
+                <Location city={"Buenos Aires"}/>
+                <WeatherData data={data}/>
+            </div>
+        )
+    }
+}
 
 export default WeatherLocation;
