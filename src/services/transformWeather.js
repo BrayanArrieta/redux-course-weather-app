7061183914792fbead24d6a35897f7ef1793f0a1
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-07-14 17:07:53 -0600

    Render the specific icon for the weather

--- src/services/transformWeather.js
+++ src/services/transformWeather.js
@@ -1,17 +1,39 @@
 import convert from 'convert-units';
+import {
+    THUNDER,
+    DRIZZLE,
+    RAIN,
+    SNOW,
+    SUN,
+    CLOUDY
+} from '../constants/weathers';
 const getTemperature = (kelvin) => {
     //convert the string to number format
     return Number(convert(kelvin).from('K').to('C').toFixed(2));
 }
 
-const getWeatherState = (weather) => {
-    return "sun";
+const getWeatherState = weather => {
+    const {id} = weather[0];
+    if (id < 300) {
+        return THUNDER;
+    } else if (id < 400) {
+        return DRIZZLE;
+    } else if (id < 600) {
+        return RAIN;
+    } else if (id < 700) {
+        return SNOW;
+    } else if (id === 800) {
+        return SUN;
+    } else {
+        return CLOUDY;
+    }
 }
 
 const transformWeather = (weatherData) => {
+    const { weather } = weatherData;
     const {humidity, temp} = weatherData.main;
     const {speed} = weatherData.wind;
-    const weatherState = getWeatherState(this.weather);
+    const weatherState = getWeatherState(weather);
     //Convert the temperature from kelvin o celsius
     const temperature = getTemperature(temp);
     const data = {
