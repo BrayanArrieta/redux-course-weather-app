8d84b66c6eff9dc7c0523619b7ca16a14259537b
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-07-14 12:03:36 -0600

    Added constructor and onClick event

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,22 +3,42 @@ import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './styles.css';
 
-const data = {
+const data1 = {
     temperature: 21,
     weatherState: "cloud",
     humidity: 80,
     wind: "10m/s"
 }
+const data2 = {
+    temperature: 30,
+    weatherState: "cloudy",
+    humidity: 80,
+    wind: "10m/s"
+}
 
 class WeatherLocation extends Component {
+    constructor() {
+        super();
+        this.state = {
+            city: "Buenos Aires",
+            data: data1
+        }
+    }
+    handleUpdateClick = () => {
+        this.setState({
+            city: "Buenos Aires",
+            data: data2
+        });
+        console.log("updated");
+    }
     render() {
         return (
             <div className='weatherLocationCont'>
-                <Location city={"Buenos Aires"}/>
-                <WeatherData data={data}/>
+                <Location city={this.state.city}/>
+                <WeatherData data={this.state.data}/>
+                <button onClick={this.handleUpdateClick}>Update</button>
             </div>
         )
     }
 }
-
 export default WeatherLocation;
