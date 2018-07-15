0980553a66730d48a7d47942c82402b3c2446a09
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-07-15 17:04:50 -0600

    Added AppBar from Material UI and responsive design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import LocationList from './components/LocationList';
 import {Grid, Row, Col} from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 const cities = ["Buenos Aires,ar", "Bogotá,col", "San Jose,cr"];
+
 class App extends Component {
     handleSelectedLocation = (city) => {
         console.log(city);
@@ -13,14 +20,25 @@ class App extends Component {
         return (
             <Grid>
                 <Row>
-                    Titulo
+                    <AppBar position="static">
+                        <Toolbar>
+                            <IconButton className='menuButton' color="inherit" aria-label="Menu">
+                                <MenuIcon/>
+                            </IconButton>
+                            <Typography variant="title" color="inherit" className='flex'>
+                                Weather App
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
                 </Row>
                 <Row>
                     <Col xs={12} md={6}>
                         <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}/>
                     </Col>
                     <Col xs={12} md={6}>
-                        <div className='detail'></div>
+                        <Paper elevation={4}>
+                            <div className='detail'></div>
+                        </Paper>
                     </Col>
                 </Row>
             </Grid>
