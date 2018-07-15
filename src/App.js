471335f6f1bb83d6238f49339406897cf46a2e3e
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-07-15 15:53:51 -0600

    Added the library react-flexbox-grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import LocationList from './components/LocationList';
-
+import {Grid, Row, Col} from 'react-flexbox-grid';
 const cities = ["Buenos Aires,ar", "Bogotá,col", "San Jose,cr"];
 class App extends Component {
     handleSelectedLocation = (city) => {
-      console.log(city);
-      
+        console.log(city);
+
     }
     render() {
+
         return (
+
             <div className="App">
                 <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}/>
             </div>
+
         );
     }
 }
