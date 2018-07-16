6635368f5fb65bd8bacac73ce6a708f7e94a3bfe
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-07-16 11:52:19 -0600

    Passed the city to the Forecast Extended component

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 class ForecastExtended extends Component {
+
+    constructor() {
+        super();
+    }
     render() {
+        const {city} = this.props;
         return (
             <div>
-                Extended 
+                Extended {city}
             </div>
         );
     }
 }
-
+ForecastExtended.propTypes = {
+    city: PropTypes.string.isRequired
+};
 export default ForecastExtended;
