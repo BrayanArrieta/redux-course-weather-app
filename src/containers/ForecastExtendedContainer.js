59a64b7a6745c73e2ed40ad5d73a70de0715af39
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-07-16 22:45:59 -0600

    Refactor to ForecastExtendedContainer component

---
+++ src/containers/ForecastExtendedContainer.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import ForecastExtended from '../components/ForecastExtended';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+class ForecastExtendedContainer extends Component {
+    render() {
+        const {city} = this.props;
+        return ((city
+            ? <ForecastExtended city={city}/>
+            : null));
+    }
+}
+ForecastExtendedContainer.propTypes = {
+    city: PropTypes.string.isRequired
+};
+//get the state with the value that was send as parameter
+const mapStateToProps = (state) => ({city: state.city})
+// for connect react and redux convert the class and connect to the store and
+// return a new class with this specifications export the new class connected to
+// the store and redux
+export default connect(mapStateToProps, null)(ForecastExtendedContainer);
