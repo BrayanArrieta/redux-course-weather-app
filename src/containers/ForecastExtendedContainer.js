59f1f1694eebe54babe08bf55df4471ae3153c8b
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-07-17 15:21:10 -0600

    Pattern selector

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ForecastExtended from '../components/ForecastExtended';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {getForecastDataFromCities,getCity} from '../reducers';
 class ForecastExtendedContainer extends Component {
     render() {
         const {city, forecastData} = this.props;
@@ -14,9 +15,11 @@ ForecastExtendedContainer.propTypes = {
     city: PropTypes.string.isRequired,
     forecastData: PropTypes.array
 };
-//get the state with the value that was send as parameter
-//receive the state
-const mapStateToProps = ({city,cities}) => ({city: city, forecastData: cities[city] && cities[city].forecastData})
+//get the state with the value that was send as parameter receive the state
+const mapStateToProps = (state) => ({
+    city: getCity(state),
+    forecastData: getForecastDataFromCities(state)
+})
 // for connect react and redux convert the class and connect to the store and
 // return a new class with this specifications export the new class connected to
 // the store and redux
